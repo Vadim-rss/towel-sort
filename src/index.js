1352551d7f61874809971b2a8d24a5a6d9c825e5
Vadim-rss <55102312+Vadim-rss@users.noreply.github.com>
2021-03-15 20:41:37 +0300

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+module.exports = function towelSort(matrix) {
+  if (arguments.length === 0) return []
 
-// You should implement your task here.
+  const prepMatrix = matrix.map((e, i, a) => {
+    return i % 2 ? a[i].reverse() : e
+  })
 
-module.exports = function towelSort (matrix) {
-  return [];
+  return prepMatrix.flat()
 }
